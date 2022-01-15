d148e240b6bd773e25e714446352fa8a1b675277
Willie Pai <PaisWillie@gmail.com>
2022-01-15 16:09:28 -0500

    Added GIFs

--- rest-day/src/App.js
+++ rest-day/src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
-import AppBar from './components/AppBar';
+import "./App.css";
+import AppBar from "./components/AppBar";
+import { Route } from "react-router-dom";
+import WorkoutSchedule from "./components/Workout Schedule/WorkoutSchedule";
 
 function App() {
-  return (
-    <AppBar />
-  );
+    return (
+        <div>
+            <AppBar />
+            <Route path="/workout-schedule">
+                <WorkoutSchedule />
+            </Route>
+        </div>
+    );
 }
 
 export default App;
