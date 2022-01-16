9d96d6c6fb09c7f0e2c09d76fbbecc8e318269b0
Lucas Chen <lucas.chen2016@gmail.com>
2022-01-16 01:38:46 -0500

    Updates

---
+++ precache-manifest.d1c501474ea245e0ae2064835c075092.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc8d3ed351e29bf1b9d0b55bd0da2a21",
+    "url": "/restday/index.html"
+  },
+  {
+    "revision": "d860b52b88d2165d1421",
+    "url": "/restday/static/css/main.7dba0fb8.chunk.css"
+  },
+  {
+    "revision": "0c418d271465e9d8b0d4",
+    "url": "/restday/static/js/2.f49daaeb.chunk.js"
+  },
+  {
+    "revision": "4fbb6499372908a13a0489137d1a6ea2",
+    "url": "/restday/static/js/2.f49daaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d860b52b88d2165d1421",
+    "url": "/restday/static/js/main.6c94abe2.chunk.js"
+  },
+  {
+    "revision": "94ac433b2e045b141716",
+    "url": "/restday/static/js/runtime-main.9a1a86df.js"
+  }
+]);
